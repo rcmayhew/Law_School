d86726d907336b2be80ff2222d8843ba46c6002f
Robert <rcmayhewii@gmail.com>
2018-01-31 14:32:20 -0800

    added a second graph for full time

--- src/graphing.py
+++ src/graphing.py
@@ -1,7 +1,9 @@
+import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
-from matplotlib import pyplot as plt
-import os
 import pathlib
+import warnings
+warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
 
 """
 this is the module to create the graphs that lists the different pay scales for recently graduated lawyers
@@ -9,37 +11,50 @@ this is the module to create the graphs that lists the different pay scales for 
 
 
 def law_graph(data, name):
-    # unemployed = data.parse().UnEmployedSeekingNumber
+    # lists for each feature
     gradtotals = data.parse().TotalGraduatesNumber
     employedtotals = data.parse().Total
+    unknowntotals = data.parse().EmploymentStatusUnknownNumber
+    schooltotals = data.parse().EmployedLawSchoolNumber
+    gradschooltotals = data.parse().EmployedPursuingGraduateDegreeNumber
 
+    # sum of the list of the feature
+    total_school = sum(schooltotals) + sum(gradschooltotals)
     total_graduated = sum(gradtotals)
-    total_employed = sum(employedtotals)
+    total_employed = sum(employedtotals) - total_school
+    total_unknown = sum(unknowntotals)
 
     """lists the indices of the columns that contains the proper info"""
-    pay_total_indices = [data.parse().columns.get_loc("26-50"),
+    pay_total_indices = [data.parse().columns.get_loc("Solo"),
+                         71,    # location of the total of 2-10K a year
+                         76,    # location of the total of 11-25K a year
+                         data.parse().columns.get_loc("26-50"),
                          data.parse().columns.get_loc("51-100"),
                          data.parse().columns.get_loc("101-250"),
                          data.parse().columns.get_loc("251-500"),
                          data.parse().columns.get_loc("501-PLUS"),
                          data.parse().columns.get_loc("Unknown")]
-    """This is the list of hte number of lawyers in each bucket"""
+    """This is the list of the number of lawyers in each bucket"""
     pay_total_value = []
     for index in pay_total_indices:
         pay_total_value.append(sum(data.parse().ix[:, index]))
-    pay_scale = ["26K-50K", "51K-100K", "101K-250K", "251K-500K",
-                 "501K Plus", "Unknown"]
+    pay_scale = ["Solo", "2K-10K", "11K-25K", "26K-50K", "51K-100K",
+                 "101K-250K", "251K-500K", "501K Plus", "Unknown"]
 
     """
     This is the list of employed vs unemployed. It is here to show
     there is a discrepancy between the number of report salaries and 
     the reported employment numbers.
     """
-    second_list = [total_employed, total_graduated - total_employed]
-    second_args = ["Total Employed", "Total Unemployed"]
+    total_known = sum(pay_total_value)
+    second_list = [total_employed, total_known, total_graduated
+                   - total_employed - total_unknown, total_school,
+                   total_unknown]
+    second_args = ["Total Employed", "Known Pay", "Total Unemployed",
+                   "Still in School", "Unknown"]
 
     """starts the figure for both graphs"""
-    fig = plt.figure(1, figsize=(15, 5))
+    fig = plt.figure(1, figsize=(20, 5))
     plt.suptitle(name)
     """this is the pay bucket graph"""
     plt.subplot(1, 2, 1)
@@ -65,3 +80,79 @@ def law_graph(data, name):
     pathlib.Path(dic).mkdir(parents=True, exist_ok=True)
     fig.savefig(file)
     plt.show()
+
+
+def FTLT_graph(data, name):
+    # lists for each feature
+    gradtotals = data.parse().TotalGraduatesNumber
+    index = data.parse().columns.get_loc("Total-FTLT")
+    employedtotals = data.parse().ix[:, index]
+    unknowntotals = data.parse().EmploymentStatusUnknownNumber
+    schooltotals = data.parse().EmployedLawSchoolNumber
+    gradschooltotals = data.parse().EmployedPursuingGraduateDegreeNumber
+    bartotals = data.parse().EmployedBarPassageRequiredNumber
+
+    # sum of the list of the feature
+    total_school = sum(schooltotals) + sum(gradschooltotals)
+    total_graduated = sum(gradtotals)
+    total_employed = sum(employedtotals) - total_school
+    total_bar = sum(bartotals)
+
+    """lists the indices of the columns that contains the proper info"""
+    pay_total_indices = [data.parse().columns.get_loc("Solo-FTLT"),
+                         data.parse().columns.get_loc("2-10-FTLT"),
+                         data.parse().columns.get_loc("11-25-FTLT"),
+                         data.parse().columns.get_loc("26-50-FTLT"),
+                         data.parse().columns.get_loc("51-100-FTLT"),
+                         data.parse().columns.get_loc("101-250-FTLT"),
+                         data.parse().columns.get_loc("251-500-FTLT"),
+                         data.parse().columns.get_loc("501-FTLT"),
+                         data.parse().columns.get_loc("Unknown-FTLT")]
+    """This is the list of the number of lawyers in each bucket"""
+    pay_total_value = []
+    for index in pay_total_indices:
+        pay_total_value.append(sum(data.parse().ix[:, index]))
+    pay_scale = ["Solo", "2K-10K", "11K-25K", "26K-50K", "51K-100K",
+                 "101K-250K", "251K-500K", "501K Plus", "Unknown"]
+
+    """
+    This is the list of employed vs unemployed. It is here to show
+    there is a discrepancy between the number of report salaries and 
+    the reported employment numbers.
+    """
+
+    non_bar = total_employed - total_bar
+    # pay_total_value[1] -= non_bar
+
+    total_known = sum(pay_total_value)
+    second_list = [total_employed, total_known, non_bar, total_graduated]
+    second_args = ["Total Employed", "Known Pay", "Total Working not as lawyer", "Total Graduated"]
+
+    """starts the figure for both graphs"""
+    fig = plt.figure(1, figsize=(20, 5))
+    plt.suptitle("FTLT %s" % (name))
+    """this is the pay bucket graph"""
+    plt.subplot(1, 2, 1)
+    xs = [i for i, _ in enumerate(pay_scale)]
+    plt.bar(xs, pay_total_value)
+    plt.ylabel("# of Lawyers")
+    plt.xlabel("Employment pay")
+    plt.title("Distribution of Reported Lawyer Pay")
+    plt.xticks([i for i, _ in enumerate(pay_scale)], pay_scale)
+    plt.autoscale(True)
+    """this is the employed graph"""
+    plt.subplot(1, 2, 2)
+    xss = [i for i, _ in enumerate(second_args)]
+    plt.bar(xss, second_list)
+    plt.ylabel("# of Lawyers")
+    plt.xlabel("Employment status")
+    plt.title("Employed Lawyers")
+    plt.xticks([i for i, _ in enumerate(second_args)], second_args)
+    """creates the files and the dir in case they have not been created"""
+    dic = "Graph/"
+    suf = ".png"
+    file = "%s%s%s" % (dic, name, suf)
+    pathlib.Path(dic).mkdir(parents=True, exist_ok=True)
+    fig.savefig(file)
+    plt.show()
+
--- src/main.py
+++ src/main.py
@@ -6,15 +6,23 @@ the employment opportunities of the graduates.
 import pandas as pd
 import graphing as gr
 
-data_16_file = 'Data\EmploymentSummary-2016.xlsx'
-data_16 = pd.ExcelFile(data_16_file)
-gr.law_graph(data_16, "data_2016")
-
 data_17_file = 'Data\EmploymentSummary-2017.xlsx'
 data_17 = pd.ExcelFile(data_17_file)
-gr.law_graph(data_17, "data_2017")
+gr.FTLT_graph(data_17, "data_2017")
+
+data_16_file = 'Data\EmploymentSummary-2016.xlsx'
+data_16 = pd.ExcelFile(data_16_file)
+# gr.law_graph(data_16, "data_2016")
 
 """
 obviously there is a difference between the reported data and the
 actual salary data. Nearly three quarters didn't report income data
 """
+
+"""
+lets start taking a look at a correlation between school ranking 
+and money after college. Then after I wast to look at pay after
+school based on FirstLargestEmployment.
+"""
+# schools = data_17.parse().SchoolName
+# print(schools)
